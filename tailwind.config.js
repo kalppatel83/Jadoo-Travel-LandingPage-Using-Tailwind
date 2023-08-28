2e8c3f13d8ac3f81c6c2969e7632b1a21047f094
Kalp Patel <kalp_patel@rocketmail.com>
2023-08-28 14:13:32 +0530

    Added 3 more sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,26 @@ module.exports = {
         secondary: "#5E6282",
         orange: "#DF6951",
         beige: "#FFF1DA",
-        // white: "#f3f3f3",
+        offWhite: "#f5f5f5",
+        customBlack: "#080809",
+        purple: "#8A79DF",
+        lightBlue: "#59B1E6",
+        pinkish: "#DFD7F9",
+        lightOrange: "#FF946D",
       },
     },
     dropShadow: {
       sm: "0 8px 8px rgba(223, 105, 81, 0.50)",
+    },
+    boxShadow: {
+      lg: "10px 0px 10px 0px rgba(89, 177, 230, 1)",
+      "3xl": "0 35px 35px rgba(0, 0, 0, 0.25)",
+    },
+    blur: {
+      "3xl": "130px",
+    },
+    lineHeight: {
+      12: "4rem",
     },
   },
   plugins: [],
